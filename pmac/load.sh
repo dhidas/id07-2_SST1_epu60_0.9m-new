8b37a314f3533d2ad0d05cba940bf161b8b35b42
Dean Andrew Hidas <dhidas@bnl.gov>
2025-02-28 16:19:45 -0500

    load

--- pmac/load.sh
+++ pmac/load.sh
@@ -1,3 +1,3 @@
 #dprestore 10.0.161.24 Download.pmc
-#dprestore 10.0.161.26 Download.pmc  # Test PMC
-dprestore 192.6.94.5 Download.pmc
+dprestore 10.0.161.25 Download.pmc  # Test PMC
+#dprestore 192.6.94.5 Download.pmc
