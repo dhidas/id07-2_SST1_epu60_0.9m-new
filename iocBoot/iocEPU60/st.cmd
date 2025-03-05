0fbaf74701dd7c059cfb547205f238c1cb54cf9e
Dean Andrew Hidas <dhidas@bnl.gov>
2025-03-05 15:50:22 -0500

    move the new stuff to attic for now

--- iocBoot/iocEPU60/st.cmd
+++ iocBoot/iocEPU60/st.cmd
@@ -28,11 +28,48 @@ EPU60_registerRecordDeviceDriver pdbbase
 
 pmacAsynIPConfigure("P0", $(PMAC1_IP))
 pmacCreateController("PMAC1", "P0", 0, 12, 50, 500)
+pmacCreateAxis("PMAC1", 1)
+pmacCreateAxis("PMAC1", 3)
+pmacCreateAxis("PMAC1", 5)
+pmacCreateAxis("PMAC1", 6)
+pmacCreateAxis("PMAC1", 7)
+pmacCreateAxis("PMAC1", 8)
+pmacCreateAxis("PMAC1", 9)
+pmacCreateAxis("PMAC1", 10)
+
+# Create CS (ControllerPort, Addr, CSNumber, CSRef, Prog)
+pmacAsynCoordCreate("P0", 0, 1, 0, 10)   # Gap
+pmacAsynCoordCreate("P0", 0, 2, 1, 12)   # Phase
+pmacAsynCoordCreate("P0", 0, 6, 2, 14)   # Fly
+pmacAsynCoordCreate("P0", 0, 6, 3, 15)   # Fly
+
+# Configure CS (PortName, DriverName, CSRef, NAxes)
+drvAsynMotorConfigure("PMAC-GAP", "pmacAsynCoord", 0, 9)
+drvAsynMotorConfigure("PMAC-PHASE", "pmacAsynCoord", 1, 9)
+drvAsynMotorConfigure("PMAC-FLY", "pmacAsynCoord", 2, 9)
+drvAsynMotorConfigure("PMAC-FLYb", "pmacAsynCoord", 3, 9)
+
+# Set scale factor (CSRef, axis, stepsPerUnit)
+pmacSetCoordStepsPerUnit(0, 6, 5)
+pmacSetCoordStepsPerUnit(1, 6, 5)
+pmacSetCoordStepsPerUnit(2, 6, 1)
+pmacSetCoordStepsPerUnit(3, 6, 1)
+
+# Set Idle and Moving poll periods (CSRef, PeriodsMilliSeconds)
+pmacSetCoordIdlePollPeriod(0, 500)
+pmacSetCoordMovingPollPeriod(0, 100)
+pmacSetCoordIdlePollPeriod(1, 500)
+pmacSetCoordMovingPollPeriod(1, 100)
+pmacSetCoordIdlePollPeriod(2, 500)
+pmacSetCoordMovingPollPeriod(2, 100)
+pmacSetCoordIdlePollPeriod(3, 500)
+pmacSetCoordMovingPollPeriod(3, 100)
 
 ## Load record instances
 cd "${TOP}/db"
-dbLoadTemplate("gap.substitutions", "SYS=$(sys),DEV=$(dev),PORT=P0")
-#dbLoadRecords "EPU60Version.db", "user=dhidas"
+dbLoadTemplate("interlock.substitutions", "SYS=$(sys),DEV=$(dev),PORT=P0")
+dbLoadTemplate("motor.substitutions", "SYS=$(sys),DEV=$(dev),PORT=P0,MOTORPORT=PMAC1")
+dbLoadTemplate("gap.substitutions", "SYS=$(sys),DEV=$(dev),PORT=P0,COORDPORT=PMAC-GAP")
 
 
 cd "${TOP}/iocBoot/${IOC}"
